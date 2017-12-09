5b665ea1f3f83f3f811b75346755f2d61d1a97a6
nttungub <nttungub@gmail.com>
2017-12-09 21:34:04 +0700

    Create build_ubuntu.sh

---
+++ build_ubuntu.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev -y
+mkdir build
+cd build
+cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+make install
+cp ../config.txt bin/
